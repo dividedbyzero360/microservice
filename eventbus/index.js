9052abbbac366bbbe8324d10d981bf450ad7ae82
dividedbyzero360 <wastabirrajveealamsa_cse10@its.edu.in>
2020-10-31 14:08:01 -0400

    added kubernetes deployment and service files for each service

--- eventbus/index.js
+++ eventbus/index.js
@@ -12,10 +12,10 @@ const events = [];
 app.post("/events", (req, res) => {
   const event = req.body;
   events.push(event);
-  axios.post("http://localhost:4000/events", event); //Post
-  axios.post("http://localhost:4001/events", event); //Comments
-  axios.post("http://localhost:4002/events", event); //Query
-  axios.post("http://localhost:4003/events", event); //Moderation
+  axios.post("http://posts-clusterip-srv:4000/events", event); //Post
+  axios.post("http://comments-srv:4001/events", event); //Comments
+  axios.post("http://query-srv:4002/events", event); //Query
+  axios.post("http://moderation-srv:4003/events", event); //Moderation
 
   res.send({ status: "OK" });
 });
